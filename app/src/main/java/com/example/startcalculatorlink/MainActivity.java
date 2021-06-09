df0fd17a134a602d81cdb4e380eb5d47ad6647c6
andrey <a.serbin@litebox.ru>
2021-06-09 15:41:30 +0400

    добавлена кнопка для запуска калькулятора по интент-фильтру

--- app/src/main/java/com/example/startcalculatorlink/MainActivity.java
+++ app/src/main/java/com/example/startcalculatorlink/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.startcalculatorlink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.launch).setOnClickListener(v -> {
+            Uri uri = Uri.parse("intentstest://calc");
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(Intent.createChooser(browserIntent,null));
+        });
     }
 }
